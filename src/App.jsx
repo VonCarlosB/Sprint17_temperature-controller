16e95dad4f8d90e84eafdf5c8eaf9d088d51fb05
CarlosBF <cborrellf@outlook.es>
2025-06-22 13:54:36 +0200

    Implemented localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ import TemperatureControls from './components/TemperatureControls'
 import HistoryList from './components/HistoryList'
 
 export default function App() {
+  if(localStorage.getItem('history') === null){
+    localStorage.setItem('history', '[]')
+  }
+
   const [temp, setTemp] = useState(20)
-  const [history, setHistory] = useState([])
+  const [history, setHistory] = useState(JSON.parse(localStorage.getItem('history')))
+
   const increaseTemp = () => {
     setTemp(temp+1)
     addEntry(temp+1)
@@ -23,23 +28,30 @@ export default function App() {
         temp
       }
       setHistory([...history, newEntry])
+      localStorage.setItem('history', JSON.stringify([...history, newEntry]))
     }, 500);
   }
   const reset = () => {
     setTemp(20)
     setHistory([])
+    localStorage.setItem('history', '[]')
   }
+
   return (
     <>
       <div className="app">
         <h1>Controlador de temperatura</h1>
         <TemperatureDisplay temp={ temp }/>
         <div className="botonera">
-          <TemperatureControls increaseTemp={increaseTemp} decreaseTemp={decreaseTemp} reset={reset}/>
+          <TemperatureControls 
+            increaseTemp={ increaseTemp } 
+            decreaseTemp={decreaseTemp} 
+            reset={reset}
+          />
         </div>
         <h3>Historial</h3>
         <ul>
-          <HistoryList history={history}/>
+          <HistoryList history={ history }/>
         </ul>
       </div>
     </>
